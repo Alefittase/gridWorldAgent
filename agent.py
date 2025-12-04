13d2dc53ee23772cc8e517903554fa4093144b26
Ali Nasrollahour <alefittase@Alis-MacBook-Pro.local>
2025-12-04 17:00:55 +0530

    added the states creation and mdp successor determination functionss

---
+++ agent.py
@@ -0,0 +1,49 @@
+GRID_MAP = [
+    ["S","_","_","X","_"],
+    ["_","X","_","_","_"],
+    ["_","_","X","_","_"],
+    ["X","_","_","_","G"],
+    ["_","_","X","_","_"],
+]
+
+ACTIONS = ["U", "R", "D", "L"]
+ACTION_DELTA = {"U":(-1,0),"R":(0,1),"D":(1,0),"L":(0,-1)}
+
+def build_status(grid):
+    states = []
+    pos2idx = {}
+    is_obstacle = {}
+    is_goal = {}
+    start = None
+    H = len(grid)
+    W = len(grid[0])
+    for i in range(H):
+        for j in range(W):
+            c = grid [i][j]
+            if c == "X":
+                is_obstacle[(i,j)] = True
+                continue
+            index = len(states)
+            states.append((i,j))
+            pos2idx[(i,j)] = index
+            is_obstacle[(i,j)] = false
+            if c == "G":
+                is_goal[(i,j)] = True
+            if c == "S":
+                start = (i,j)
+    return states, pos2idx, is_obstacle, is_goal, start, H, W
+
+def mdp_successor(pos, action, grid, pos2idx, is_obstacle, is_goal, H, W):
+    i, j = pos
+    di, dj = ACTION_DELTA[action]
+    ni, nj = i+di, j+dj
+
+    if not (0 <= ni < H and 0 <= nj < W):
+        return pos, -5
+    if grid[ni][nj] == "X":
+        return pos, -5
+    if grid[ni][nj] == "G":
+        return (ni, nj), 10
+    return (ni, nj), -1
+
+def 
